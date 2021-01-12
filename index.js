9e5587e6a8cecafdb5f86906574877b6c4f78c02
Xiang Wang <ramwin@qq.com>
2021-01-12 11:32:56 +0800

    create init package

---
+++ index.js
@@ -0,0 +1,13 @@
+// Xiang Wang @ 2021-01-12 11:18:25
+
+
+exports.desc = "ramwin's package"
+exports.long_desc = `\
+This is a very long sentence that \
+you can use is to test whether \
+your web page can custom it's apperance and \
+is available for a very long sentence. \
+Use
+  .ellipsis-text {white-space: nowrap; overflow: hidden; text-overflow: ellipsis; }
+  .ellipsis-text:hover { overflow: visible; }
+to only show '...' in the html.`
---
+++ test/test.js
@@ -0,0 +1,5 @@
+// Xiang Wang @ 2021-01-12 11:18:49
+
+const r = require('ramwin')
+console.info(r.desc)
+console.info(r.long_desc)
